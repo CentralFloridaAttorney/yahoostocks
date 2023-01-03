f7cb5a3b25d14fb2824921e9bd90ed015ca03836
CentralFloridaAttorney <attorney@centralfloridaattorney.net>
2023-01-03 15:38:44 -0500

    reorganize

--- python/yahoostocks/yahoostock_BAD.py
+++ python/yahoostocks/yahoostock_BAD.py
@@ -1,26 +1,21 @@
 import json
 import math
-import urllib.request
-from urllib.error import URLError
 
 import numpy
 import pandas
 import torchvision.transforms as transforms
 
-from python.yahoof.yahoofinance import YahooFinancials
 from python.yahoostocks.classifier import Classifier
+from python.yahoostocks.stock_data_getter import StockData
 
-tickerX = 'LIC'
-
-BASE_DIR = '../../data/stocks/'
+tickerX = 'HRL'
+BASE_DIR = '../data/stocks/'
+START_DATE = '1900-01-01'
+END_DATE = '2022-06-02'
 
 
 class YahooStock:
     def __init__(self, init_ticker="F"):
-        """
-        YahooStock is a data object.  This implementation loads stock market data for a publicly traded company and makes the data available for use in machine learning projects.
-        :param init_ticker: stock market ticker symbol for a publicly traded company
-        """
         self.transform = transforms.Compose(
             [transforms.ToTensor(),
              transforms.Normalize((0.5,), (0.5,))])
@@ -29,8 +24,19 @@ class YahooStock:
         self.validation_loader = None
         self.training_loader = None
         self.ticker = init_ticker
-        self.text_string = self.load_ticker()
-        self.json_data = self._load_json_data()
+        try:
+            text_file = open(BASE_DIR + self.ticker + '.json', 'r')
+            self.text_string = text_file.read()
+            text_file.close()
+        except FileNotFoundError:
+            self.load_ticker()
+        except ValueError:
+            self.load_ticker()
+        except Exception as err:
+            print('jsonstock.__init__ error: ' + str(err))
+
+        self.text_string = self.text_string.replace('\n', '')
+        self.json_data = json.loads(self.text_string)
         prices_a = self.json_data[self.ticker]['prices']
         these_prices = []
         for price in prices_a:
@@ -39,61 +45,13 @@ class YahooStock:
         self.data_frame = self.price_frame.to_numpy()
 
     def load_ticker(self):
-        # if self.ticker.json exists then load the file
-        yf = YahooFinancials(self.ticker)
-        try:
-            # stock_file = open(BASE_DIR+self.ticker+'.json', 'r+')
-            # file_string = stock_file.read()
-            # file_string = file_string.replace('\n', ' ')
-            uh = urllib.request.urlopen('file:' + BASE_DIR + self.ticker + '.json')
-            this_data = uh.read()
-
-            print('Retrieved', len(this_data), 'characters')
-            text_string = 'default'
-            text_string = str(json.loads(this_data.decode("utf-8")))
-            text_data = pandas.DataFrame(text_string)
-
-            # text_string = json.loads(file_string.de)
-            print('*** Loaded stock data from file: ' + self.ticker)
-        except URLError:
-            stock_dict = yf.get_historical_price_data(start_date='1900-01-01', end_date='2022-06-02',
-                                                      time_interval='daily')
-            text_data = pandas.DataFrame(stock_dict)
-
-            text_file = open(BASE_DIR + self.ticker + '.json', 'w')
-            text_string = str(stock_dict)
-            text_string = text_string.replace('\n', ' ')
-            text_file.write(text_string)
-            text_file.close()
-            print('WARNING: Loaded stock data directly from yahoo: ' + self.ticker)
-        # text_string = json.dumps(text_string)
-        print('*** finished loading stock data: ' + self.ticker)
-        return text_string
-
-    def _load_json_data(self):
-        # the file to be converted to
-        # json format
-        filename = BASE_DIR + self.ticker + '.json'
-
-        # dictionary where the lines from
-        # text will be stored
-        json_dict = {}
-
-        # creating dictionary
-        with open(filename) as fh:
-            for line in fh:
-                # reads each line and trims of extra the spaces
-                # and gives only the valid words
-                command, description = line.strip().split(None, 1)
-
-                json_dict[command] = description.strip()
-
-        # creating json file
-        # the JSON file is named as test1
-        out_file = open("test1.json", "w")
-        json.dump(json_dict, out_file, indent=4, sort_keys=False)
-        out_file.close()
-        return json_dict
+        yf = StockData(self.ticker)
+        self.text_string = yf.get_past_data(start_date=START_DATE, end_date=END_DATE, time_interval='daily')
+        print('WARNING: Loaded data directly from yahoo')
+        self.text_string = json.dumps(self.text_string)
+        text_file = open(BASE_DIR + self.ticker + '.json', 'w+')
+        text_file.write(self.text_string)
+        text_file.close()
 
     def get_price_data(self, column_name):
         try:
@@ -119,13 +77,13 @@ class YahooStock:
 
     @staticmethod
     def get_classification(data_column, threshold_value=0, lagging_average=None):
-        """Returns 1.0 in a column when column_data exceeds the threshold_value, if the value is below threshold_value
-        then 0.0 is added
+        """
+        This method returns a list where the value is 1 when price_data exceeds the threshold_value.  The values in the list correspond to the order of the values in price_frame.
 
         Parameters
         ----------
         data_column : DataFrame
-            The options column to be added to file_data
+            The data column to be added to file_data
         threshold_value : float
             The minimum value for a true result
 
@@ -137,7 +95,7 @@ class YahooStock:
             :param threshold_value:
             :param lagging_average:
         """
-        return Classifier.from_threshold(data_column, threshold_value)
+        return Classifier().from_threshold(data_column, threshold_value)
 
     @staticmethod
     def get_test_train_split(_data, _split_col, _batch_size, _train_ratio):
@@ -150,7 +108,7 @@ class YahooStock:
         except TypeError:
             _data = numpy.array(_data)
             _num_rows, _num_cols = _data.shape
-        _extra_rows_1 = _num_rows % (_batch_size * _train_ratio)
+        _extra_rows_1 = (_num_rows) % (_batch_size * _train_ratio)
         _extra_rows_2 = (_num_rows % _batch_size) * _train_ratio
         _extra_rows_3 = (_num_rows * _train_ratio) % _batch_size
 
@@ -163,7 +121,7 @@ class YahooStock:
         _x_train = _data[0:_num_x, 0:_split_col]
         _x_target = _data[0:_num_x, _split_col:_split_col]
         _x_target = numpy.resize(_x_target, [_num_x, 1])
-        _y_train = _data[_num_x:(_num_x + _num_y), 0:_split_col]
+        _y_train = _data[_num_x:(_num_x + _num_y), 0:(_split_col)]
         _y_target = _data[_num_x:(_num_x + _num_y), _split_col:_split_col]
         _y_target = numpy.resize(_y_target, [_num_y, 1])
         return _x_train, _x_target, _y_train, _y_target  # , _x_target, _y_target
@@ -178,15 +136,21 @@ class YahooStock:
 
 if __name__ == '__main__':
     stockItem = YahooStock(tickerX)
-    multi_tickers = []
-    tickers = ['LIC']
+
+    tickers = ['KO', 'CSX']
     for ticker in tickers:
-        multi_tickers.append(YahooStock(ticker))
+        YahooStock(ticker)
 
     stockItem.drop_column(7)
     col1 = stockItem.get_price_data(4)
-    classification = stockItem.get_classification_greater_prior(2, 7)
+    # classification = stockItem.get_classification_greater_prior(2, 7)
+    classification = Classifier.get_classification_greater_prior(_dataframe=stockItem.price_frame, _col_name='high',
+                                                                 _days_before=2)
     stockItem.add_column(classification)
     data = stockItem.price_frame.to_numpy()
     x_train, y_train, x_test, y_test = stockItem.get_test_train_split(data, 3, 7, .87)
+    # the following code returns a list where the value is 1 when the price data exceeds the threshold_value
+    average_value = Classifier.get_average_col_value(_dataframe=col1, _col_num=0)
+    indices_above_threshold = Classifier.from_threshold(_numpy_column=col1,
+                                                        _threshold_value=(average_value + (average_value * .2)))
     print('Finished!')
